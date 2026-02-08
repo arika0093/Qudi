e869354546dcc533e23cd2837e6e466b6fc6d5a1
Arika Ishinami <delete0093@gmail.com>
2026-02-08 23:16:14 +0900

    Add example readme project and update solution file

---
+++ examples/Qudi.Example.Readme/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.DependencyInjection;
+using Qudi;
+
+var services = new ServiceCollection();
+
+// ✅️ register services marked with Qudi attributes (see below)
+services.AddQudiServices();
+
+var provider = services.BuildServiceProvider();
+var pokemons = provider.GetServices<IPokemon>();
+foreach (var pokemon in pokemons)
+{
+    pokemon.DisplayInfo();
+}
+
+// ------ Declare services ------
+public interface IPokemon
+{
+    string Name { get; }
+    IEnumerable<string> Types { get; }
+    public void DisplayInfo() =>
+        Console.WriteLine($"{Name} is a {string.Join("/", Types)} type Pokémon.");
+}
+
+[DISingleton] // ✅️ mark as singleton
+public class Altaria : IPokemon
+{
+    public string Name => "Altaria";
+    public IEnumerable<string> Types => ["Dragon", "Flying"];
+}
+
+[DITransient] // ✅️ mark as transient, too
+public class Abomasnow : IPokemon
+{
+    public string Name => "Abomasnow";
+    public IEnumerable<string> Types => ["Grass", "Ice"];
+}
