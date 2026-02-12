df328ef845fe4f929febc16cbf4579d124f54f1b
Arika Ishinami <delete0093@gmail.com>
2026-02-12 10:01:53 +0000

    feat: add visualization output configuration and files

--- examples/Qudi.Example.SimpleCase.Worker/Program.cs
+++ examples/Qudi.Example.SimpleCase.Worker/Program.cs
@@ -4,7 +4,14 @@ using Qudi.Example.Worker;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddQudiServices(conf =>
 {
-    conf.EnableVisualizationOutput();
+    conf.EnableVisualizationOutput(option =>
+    {
+        option.AddOutput("assets/visualization_output.json");
+        option.AddOutput("assets/visualization_output.dot");
+        option.AddOutput("assets/visualization_output.mermaid");
+        option.AddOutput("assets/visualization_output.dgml");
+        option.EnableConsoleOutput = true;
+    });
     conf.SetCondition(builder.Environment.EnvironmentName);
 });
 builder.Services.AddHostedService<Worker>();
